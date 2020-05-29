11a352cb32b962dcba03a155808fbe864dc6ff10
PhongCaos <=>
2020-05-29 09:57:25 +0700

    [None][1.0][Company] Add multi language

--- JSMasterBook/src/app/layout/header/header.component.ts
+++ JSMasterBook/src/app/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -6,7 +7,14 @@ import { Component, HostListener, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+    translate.use('en');
+   }
+
+   switchLanguage(language: string ) {
+        this.translate.use(language);
+   }
 
   ngOnInit(): void {
   }
