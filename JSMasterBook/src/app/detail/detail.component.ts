c304504534e8b280a8eeb6648fefdf0a5ce9024a
PhongCaos <=>
2020-06-06 14:20:08 +0700

    [None][2.1] Add Animation by using Lotties

--- JSMasterBook/src/app/detail/detail.component.ts
+++ JSMasterBook/src/app/detail/detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +9,16 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent implements OnInit {
-
+  options: AnimationOptions = {
+    path: '../../assets/tutImage/animation.json',
+  };
+    // tslint:disable-next-line:ban-types
   constructor(private translate: TranslateService) {
    }
+
+   animationCreated(animationItem: AnimationItem): void {
+    console.log(animationItem);
+  }
 
 
   ngOnInit(): void {
